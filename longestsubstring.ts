2a47ed7a4960da98788c75a30fefbdd230107619
Jessi Pavia <jessipavia@Jessis-MacBook-Air.local>
2025-08-02 00:41:10 -0700

    initialize new project for finiding media valur in winow sliding

---
+++ longestsubstring.ts
@@ -0,0 +1,8 @@
+// 340. Longest Substring with At Most K Distinct Characters
+
+// In this problem, we are given a string s and an integer k. Our task is to find the length of the longest substring (which is a contiguous block of characters in the string) within s that contains at most k distinct characters.
+
+// A distinct character means that no matter how many times the character appears in the substring, it is counted only once. For example, if k = 2, the substring "aabbc" has 3 distinct characters ('a', 'b', and 'c'), thus, it does not meet the requirement.
+
+// The goal here is to achieve this while maximizing the length of the substring. A substring could range from containing a single character up to the length of the entire string, if k is sufficiently large to cover all distinct characters in the string s.
+
---
+++ slidingWindowMedia.ts
@@ -0,0 +1,11 @@
+// collect what we have
+// window moving to the right
+// calculate the media for that current window,
+// add the median value into the final array
+// consider using a dictionary
+
+function medianSlidingWindow(nums: number[], k: number): number[] {
+    // constraints: k !> nums.length
+    
+    
+};
