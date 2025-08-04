141f224d4101064495d6170be60bf60e40fae768
Jessi Pavia <jessipavia@Jessis-MacBook-Air.local>
2025-08-03 23:37:33 -0500

    added a new solution form leetcode

--- subArray/contiguousArray.ts
+++ subArray/contiguousArray.ts
@@ -2,21 +2,21 @@
 // Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1
 function findMaxLength(nums: number[]) : number {
    // this requires a prefix sum + hash map
-   const map = new Map<number, number>();
+   const map = new Map();
    map.set(0, -1);
-   let maxLength = 0;
-   let count = 0;
+   let curr = 0, ans = 0;
+   const matchMap = new Map();
 
    for (let i = 0; i < nums.length; i++) {
-    // convert 0 to -1
-    count += nums[i] === 1 ? 1 : -1;
-    if (map.has(count)) {
-        maxLength = Math.max(maxLength, i - map.get(count)!);
+    curr += nums[i] === 1 ? 1 : -1;
+
+    if (map.has(curr)) {
+        ans = Math.max(ans, i - map.get(curr));
     } else {
-        map.set(count, i);
+        map.set(curr, i);
     }
    }
-   return maxLength;
+   return ans;
 }
 
 console.log(findMaxLength([0,1,1,1,0,1,0,0,1]));
